d2d907cd8676a304d96a248951db8b572b4d1250
DmitryP <snoops@list.ru>
2018-05-15 11:08:48 +0400

    implemented from configuration for nested attributes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,9 +6,11 @@ class PortfoliosController < ApplicationController
   
   def angular
     @angular_portfolio_items = Portfolio.angular
-  end  
+  end 
+
   def new
     @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end  
 
   def create
@@ -52,6 +54,6 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(:title, :subtitle, :body)
+    params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name])
   end  
 end
