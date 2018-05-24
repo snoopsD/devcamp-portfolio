d05b983011539a7770af10bc15c793dc22929693
DmitryP <snoops@list.ru>
2018-05-24 16:37:11 +0400

    Added guest user for current_user

--- config/application.rb
+++ config/application.rb
@@ -10,5 +10,5 @@ module Hartl
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    
+  end    
 end
